ab58854d81499cd3e7c4eb9e2d60a341a45e4bea
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-28 18:23:12 +0530

    Update terraformCD.groovy

--- src/org/template/terraformCD.groovy
+++ src/org/template/terraformCD.groovy
@@ -12,22 +12,22 @@ def call(String repoUrl, String branch = 'main', String credentialsId, String te
     terraformPlan = new terraform_plan()
     terraformApply = new terraform_apply()
     terraformDestroy = new terraform_destroy()
-
+    ManualApproval = new manualapproval()
+    
     clean.call()
     clone.call(repoUrl, branch, credentialsId)
     terraformInit.call(terraform_path)
     terraformPlan.call(terraform_path)
 
-    if (action == 'apply')
-    {
-        input message: 'Approval for infrastructure apply', ok: 'approval'
+    if (action == 'apply') {
+        manualApproval.call('Approval for infrastructure apply')
         terraformApply.call(terraform_path)
-    }
-        
     } else if (action == 'destroy') {
-     input message: 'Approval for infrastructure destroy', ok: 'approval'
+        manualApproval.call('Approval for infrastructure destroy')
         terraformDestroy.call(terraform_path)
     } else {
         error("Invalid action: ${action}. Please specify 'apply' or 'destroy'.")
     }
+
+   
 }
