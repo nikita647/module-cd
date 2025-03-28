a1d5219eabac9f9b20f3a6024d13d4b8afad2a6f
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-28 23:09:22 +0530

    Update terraformCD.groovy

--- src/org/template/terraformCD.groovy
+++ src/org/template/terraformCD.groovy
@@ -5,25 +5,25 @@ import org.terraform.*
 
 def call(String repoUrl, String branch = 'main', String credentialsId = '', String terraform_path, String message, String action) {
 
-    def clean = new cleanWs()
-    def clone = new gitClone()
-    def terraformInit = new terraform_init()
-    def terraformPlan = new terraform_plan()
-    def terraformApply = new terraform_apply()
-    def terraformDestroy = new terraform_destroy()
-    def manualApproval = new manualapproval()
+    clean = new cleanWs()
+    clone = new gitClone()
+    terraformInit = new terraform_init()
+    terraformPlan = new terraform_plan()
+    terraformApply = new terraform_apply()
+    terraformDestroy = new terraform_destroy()
+    manualApproval = new manualapproval()
     
-    clean()
-    clone(repoUrl, branch, credentialsId)
-    terraformInit(terraform_path)
-    terraformPlan(terraform_path)
+    clean.call()
+    clone.call(repoUrl, branch, credentialsId)
+    terraformInit.call(terraform_path)
+    terraformPlan.call(terraform_path)
 
     if (action == 'apply') {
         manualApproval('Approval for infrastructure apply')
-        terraformApply(terraform_path)
+        terraformApply.call(terraform_path)
     } else if (action == 'destroy') {
         manualApproval('Approval for infrastructure destroy')
-        terraformDestroy(terraform_path)
+        terraformDestroy.call(terraform_path)
     } else {
         error("Invalid action: ${action}. Please specify 'apply' or 'destroy'.")
     }
