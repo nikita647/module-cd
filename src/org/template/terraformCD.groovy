39f120eddc200eb3aba6c3f50911e687ee96736b
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-28 16:56:25 +0530

    Update terraformCD.groovy

--- src/org/template/terraformCD.groovy
+++ src/org/template/terraformCD.groovy
@@ -9,16 +9,16 @@ def call (String repoUrl, String branch='main', String credentialsId, String ter
  clone = new gitClone()
  terraformInit = new terraform_init()
  terraformPlan = new terraform_plan()
- terraformApply = new terraform_apply()
- terraformDestroy = new terraform_destroy()
+ // terraformApply = new terraform_apply()
+ // terraformDestroy = new terraform_destroy()
 
 
 clean.call()
 clone.call(repoUrl, branch, credentialsId)
 terraformInit.call(terraform_path)
 terraformPlan.call(terraform_path)
-terraformApply.call(terraform_path)
-terraformDestroy.call(terraform_path)
+// terraformApply.call(terraform_path)
+// terraformDestroy.call(terraform_path)
 
   
 }
