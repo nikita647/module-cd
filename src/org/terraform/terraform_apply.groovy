027a4bb791e7c659591b616f1c19713cd487b9be
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-29 13:15:26 +0530

    Update terraform_apply.groovy

--- src/org/terraform/terraform_apply.groovy
+++ src/org/terraform/terraform_apply.groovy
@@ -1,6 +1,8 @@
 package org.terraform
 
 def call(String terraform_path) {
+    stage('Terraform apply') {
     echo "Applying Terraform configuration in ${path}..."
     sh "terraform -chdir=${terraform_path} apply --auto-approve"
+    }
 }
