62f74b9efcc7699074ea3f6ca2935040aef5e1f7
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-29 13:19:08 +0530

    Update terraform_plan.groovy

--- src/org/terraform/terraform_plan.groovy
+++ src/org/terraform/terraform_plan.groovy
@@ -1,6 +1,8 @@
 package org.terraform
 
 def call(String terraform_path) { 
+        stage('Terraform plan') {
         echo "Running Terraform Plan in ${terraform_path}..."
         sh "terraform -chdir=${terraform_path} plan"
+        }
     }
