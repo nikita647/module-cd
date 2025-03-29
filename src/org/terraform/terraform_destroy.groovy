f7b8f7f4f99a6d862c57374f9b07f82b3667803b
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-29 13:15:59 +0530

    Update terraform_destroy.groovy

--- src/org/terraform/terraform_destroy.groovy
+++ src/org/terraform/terraform_destroy.groovy
@@ -1,6 +1,8 @@
 package org.terraform
 
 def call(String terraform_path) {
+    stage('Terraform Destroy') {
     echo "Applying Terraform configuration in ${path}..."
     sh "terraform -chdir=${terraform_path} destroy --auto-approve"
+    }
 }
